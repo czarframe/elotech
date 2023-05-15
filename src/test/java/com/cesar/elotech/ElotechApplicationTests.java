c195bdf845eccb57802ff307512021f57c47e9ec
czarframe <cesaragstincs@gmail.com>
2023-05-15 19:26:50 -0300

    First commit

---
+++ src/test/java/com/cesar/elotech/ElotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.elotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
